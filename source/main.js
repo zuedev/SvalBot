f6468478d3a1fb5cf5b3dccfe056fa1cb19a8696
zuedev <zuedev@gmail.com>
2024-04-28 17:41:36 +0100

    env var checks

--- source/main.js
+++ source/main.js
@@ -6,6 +6,18 @@ import log from "./library/log.js";
 
 log("Main boot sequence has started...");
 
+// missing required env vars check
+const requiredEnvVars = ["DISCORD_BOT_TOKEN", "MONGODB_URI", "OPENAI_API_KEY"];
+const missingEnvVars = requiredEnvVars.filter((envVar) => !process.env[envVar]);
+
+if (missingEnvVars.length)
+  throw new Error(
+    `Missing environment variables: ${missingEnvVars.join(", ")}`
+  );
+
+// set optional env vars defaults
+process.env.ENVIRONMENT ??= `unnamed-${Math.random().toString(36).slice(2)}`;
+
 await api();
 await bot();
 
