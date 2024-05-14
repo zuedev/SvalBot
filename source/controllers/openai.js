7e6083a1533a9c177aa6040ca1d3bc6fdd4d05a9
zuedev <zuedev@gmail.com>
2024-05-14 06:32:07 +0100

    move to gpt-4o

--- source/controllers/openai.js
+++ source/controllers/openai.js
@@ -51,7 +51,7 @@ export async function chatgpt({
       };
 
     const response = await openai.chat.completions.create({
-      model: "gpt-4-turbo",
+      model: "gpt-4o",
       messages: [
         { role: "system", content: personality },
         ...conversation,
